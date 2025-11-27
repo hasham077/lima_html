63a0b8b0100b4fc83a862244d848c0290c86204f
Hasham Mubeen <68544555+hasham077@users.noreply.github.com>
2025-11-27 20:24:02 +0500

    fix projects issues

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8c9d89e4bb521e595e1ca5b1d8ed1890",
+const RESOURCES = {"flutter_bootstrap.js": "079d73de01d9faeacdeda22f47d85fc9",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "f9430aa598dc12cca744a38f76553a03",
 "/": "f9430aa598dc12cca744a38f76553a03",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
-"main.dart.js": "042b77c90d55e76c0dc617cc7bae0869",
+"main.dart.js": "b38c84988acbe08d72d7f885932446ec",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
