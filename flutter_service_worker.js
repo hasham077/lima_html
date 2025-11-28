0eb00cc2c6535271f4768bfcfe31dbd84d3b471d
Hasham Mubeen <hashamsiddiqui507@gmail.com>
2025-11-28 12:49:49 +0500

    implement the tags section

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "079d73de01d9faeacdeda22f47d85fc9",
+const RESOURCES = {"flutter_bootstrap.js": "57e24bbc714c2e0395c7a2a47519e3bf",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "f9430aa598dc12cca744a38f76553a03",
 "/": "f9430aa598dc12cca744a38f76553a03",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
-"main.dart.js": "b38c84988acbe08d72d7f885932446ec",
+"main.dart.js": "e81ca6d44cd29177ad491d7691178861",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
