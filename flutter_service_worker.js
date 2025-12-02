c4689620895053808d2dea5483d794200ce72f54
Hasham Mubeen <68544555+hasham077@users.noreply.github.com>
2025-12-02 17:46:52 +0500

    new apis

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "57e24bbc714c2e0395c7a2a47519e3bf",
+const RESOURCES = {"flutter_bootstrap.js": "d743aec4596ed27a999bfdee2bbe0aba",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "f9430aa598dc12cca744a38f76553a03",
 "/": "f9430aa598dc12cca744a38f76553a03",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
-"main.dart.js": "e81ca6d44cd29177ad491d7691178861",
+"main.dart.js": "7c7aebffd2c191929cee343635e68a68",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
