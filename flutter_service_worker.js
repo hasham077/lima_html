069bc5201d15b7f75b505960f4ead66320a2ef05
Mohammad Imran Nawaz <imrannawaz288@gmail.com>
2025-12-03 15:51:15 +0500

    tasks/project inbox edit and tasks get fixed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b44b0f1e721ba40a3a882666270a5f76",
+const RESOURCES = {"flutter_bootstrap.js": "25a5a4ac6bb8c9b4f7751c530b237bdf",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "f9430aa598dc12cca744a38f76553a03",
 "/": "f9430aa598dc12cca744a38f76553a03",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
-"main.dart.js": "e6b071bc348c6f7dfd5383185d96e776",
+"main.dart.js": "eb12be42e6325dc15e86d177d60fd9c4",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
