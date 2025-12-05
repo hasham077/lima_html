d9fd3f6adbd24c6caf44a3d45cdb4e94a85d8abe
Hasham Mubeen <68544555+hasham077@users.noreply.github.com>
2025-12-05 20:02:58 +0500

    task fix and signup fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "016aad1aadec4be00b2657a61bd5ec8d",
+const RESOURCES = {"flutter_bootstrap.js": "9475d858e0604bd19514aee599d20457",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "7f5508c543e43906f9e1c898a8700dd5",
 "/": "7f5508c543e43906f9e1c898a8700dd5",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
-"main.dart.js": "2cc4722ef60a8c598f3d395185c34756",
+"main.dart.js": "e9cfa381c7ed2889a0fdb2a2ec7909ef",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
