2085f7a3eb70760ccc9ce58ed2a94e499c0ef6d5
Hasham Mubeen <68544555+hasham077@users.noreply.github.com>
2025-12-05 15:55:29 +0500

    implement url strategy and redirects

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,20 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a1afe724e3845aa282bece7d8eb2e06d",
+const RESOURCES = {"flutter_bootstrap.js": "78bde3174a24c93a58996297272df8e2",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "7f5508c543e43906f9e1c898a8700dd5",
 "/": "7f5508c543e43906f9e1c898a8700dd5",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
-"main.dart.js": "ab8027fd3d67b5b020856882678281bd",
+"main.dart.js": "18a949a445029869e189b7cc49bacda0",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "icon-192-maskable.png": "568cdde218af01ea2c2bba86f037e290",
 "icon-512-maskable.png": "bbf1f54e63ea6738b6723e4026f4ee99",
 "manifest.json": "ecf51586db3e52ef0bf4edd6116fdecb",
-"assets/NOTICES": "916399fae7a4b50c17b93408b6a611c9",
+"assets/NOTICES": "ffd60468bc6447b4e37a9b94e6201268",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.bin.json": "bfd498ea834b75600c08629f260f80b3",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
