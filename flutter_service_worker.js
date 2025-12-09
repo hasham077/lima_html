7b1d448e61d875f1bda8b377f992975eff02cfc6
Hasham Mubeen <68544555+hasham077@users.noreply.github.com>
2025-12-09 21:36:07 +0500

    reshuffle api

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "20c1c6f53c8d6da2b03a6b30eba8e81b",
+const RESOURCES = {"flutter_bootstrap.js": "56986c56c015d807a7d4954f785bc8de",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "345621f9279672556282019940bb8ef3",
 "/": "345621f9279672556282019940bb8ef3",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
-"main.dart.js": "bbf6acd9a74c1fdc3bb3ba3f226c9010",
+"main.dart.js": "f593508e2228f382287024ef5f7ff600",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
