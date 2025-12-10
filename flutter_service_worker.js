74e03bdfdcbb2bb604d504af34b13c98509b4103
Hasham Mubeen <68544555+hasham077@users.noreply.github.com>
2025-12-10 18:06:46 +0500

    resolve 2 factor issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,13 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7da36d33a7bfb73a2e1638ed3e2958fc",
+const RESOURCES = {"flutter_bootstrap.js": "73e883fd287432cefde8af285c0dcc09",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "345621f9279672556282019940bb8ef3",
 "/": "345621f9279672556282019940bb8ef3",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
-"main.dart.js": "0a09d735a2750429093ec795d4a6f3f7",
+"main.dart.js": "24428823cf8bdd06a0f425f3b483b7d4",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
