15d1f91c6486a524224a2737281415b30bbe9307
Hasham Mubeen <68544555+hasham077@users.noreply.github.com>
2025-12-18 19:56:31 +0500

    ui updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b8260c247735daa8f710a8161bd3cfb2",
+const RESOURCES = {"flutter_bootstrap.js": "078032efd14578a5f5ed83a5a97b406b",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "6fe643b06a362602302a7e71aaf52a5b",
 "/": "6fe643b06a362602302a7e71aaf52a5b",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
 "firebase-messaging-sw.js": "e4c319f2a2122050550d7605e9eeb613",
-"main.dart.js": "2e28adbec2a300fea5429b8bd40ee2ea",
+"main.dart.js": "43108d07d900a4b1b63202db637d7953",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
