ca8c38d2586fea0a33c886541dcadf37889497a8
Hasham Mubeen <imrannawaz288@gmail.com>
2025-12-26 14:19:31 +0500

    project filter pagination implementation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "325deb7ae2a381f217ac6ee7a32b9f23",
+const RESOURCES = {"flutter_bootstrap.js": "570501b582e931174fbb12c5f5f3da12",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "6fe643b06a362602302a7e71aaf52a5b",
 "/": "6fe643b06a362602302a7e71aaf52a5b",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
 "firebase-messaging-sw.js": "e4c319f2a2122050550d7605e9eeb613",
-"main.dart.js": "456f50c38d183c43c055952e45dfe599",
+"main.dart.js": "0e689da8b14c502dbaafdd5bb6ca625a",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
