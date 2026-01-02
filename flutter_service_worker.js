eb539c90f612cc8d7135c1a91fe06a6077a734da
Hasham Mubeen <imrannawaz288@gmail.com>
2026-01-02 17:05:40 +0500

    merge raheem code, heptic effect, task creation easyflow

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0bef65bbeac2d8b6c82a762c708cad57",
+const RESOURCES = {"flutter_bootstrap.js": "def200d32b64967dae24e5e6ad7c34fe",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "6fe643b06a362602302a7e71aaf52a5b",
 "/": "6fe643b06a362602302a7e71aaf52a5b",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
 "firebase-messaging-sw.js": "e4c319f2a2122050550d7605e9eeb613",
-"main.dart.js": "66dc96e95866f15d521ba15472581e49",
+"main.dart.js": "534d22f86ecadf22aef105f466eade51",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
