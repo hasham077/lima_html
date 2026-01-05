5b0468dea06664f81b95c1d0e3dcb14e6f41fb33
Hasham Mubeen <imrannawaz288@gmail.com>
2026-01-05 10:55:13 +0500

    raheem code live

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "686941185"
+    serviceWorkerVersion: "3683931225"
   }
 });
