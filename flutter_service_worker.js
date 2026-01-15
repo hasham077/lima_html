8005d78b4d0e8492da51bbd9de2adfff215bf9b3
Hasham Mubeen <imrannawaz288@gmail.com>
2026-01-15 19:14:32 +0500

    ai agent

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b6e87c59476ec70a7f42ff6ed1c7adbb",
+const RESOURCES = {"flutter_bootstrap.js": "acc632c6198d33221f8dcc5a5a75841d",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "6fe643b06a362602302a7e71aaf52a5b",
 "/": "6fe643b06a362602302a7e71aaf52a5b",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
 "firebase-messaging-sw.js": "e4c319f2a2122050550d7605e9eeb613",
-"main.dart.js": "69d323f9e3a41548fbb4c41bce556163",
+"main.dart.js": "6badf586f069b192fedc64a3b5d0d4eb",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
