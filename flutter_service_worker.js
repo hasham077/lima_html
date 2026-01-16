bf80925660a4e04a20f54db66ce3fdbfe9fb0244
Hasham Mubeen <imrannawaz288@gmail.com>
2026-01-16 14:42:25 +0500

    debug console

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "dff6664466c69bd60f8d4e69a8bf5c89",
+const RESOURCES = {"flutter_bootstrap.js": "b65d126c9a20c2a793193cac9f1d526e",
 "version.json": "14c165294551554b4eed72d165e204fa",
 "favicon.ico": "9c89ffeb2e78bb3249992facaf036525",
 "index.html": "6fe643b06a362602302a7e71aaf52a5b",
 "/": "6fe643b06a362602302a7e71aaf52a5b",
 "apple-touch-icon.png": "5397e4d0c2fc49e0aae605ad22c95205",
 "firebase-messaging-sw.js": "e4c319f2a2122050550d7605e9eeb613",
-"main.dart.js": "7a3f2a3e16561133de8ff32f931b3ba5",
+"main.dart.js": "16d3044c039f4c404c15f02f89a97784",
 "icon-192.png": "ee602ade165265417a22c9cdc4353204",
 "404.html": "147900f5b600ee8639beb65bb6e6a3d9",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
