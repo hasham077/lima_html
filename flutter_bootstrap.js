3610033406379766f775bfd1799ffb20d8913337
Imran Nawaz <imrannawaz288@gmail.com>
2026-02-20 12:56:16 +0500

    text styles changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "74108370"
+    serviceWorkerVersion: "2976390640"
   }
 });
